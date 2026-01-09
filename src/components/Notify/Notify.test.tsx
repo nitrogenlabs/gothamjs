c83acb76947a424836a063d9c27e9adedf80acc0
Giraldo Rosales <giraldo@nitrogenx.co>
2026-01-09 07:24:02 -0600

    feat: update notify tests

---
+++ src/components/Notify/Notify.test.tsx
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
+ * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
+ */
+import {render} from '@nlabs/lex/test-react';
+
+import {Notify} from './Notify.js';
+
+describe('Notify', () => {
+  it('should render without crashing', () => {
+    const {container} = render(<Notify />);
+    expect(container).toBeDefined();
+  });
+
+  it('should not display notification initially', () => {
+    const {queryByRole} = render(<Notify />);
+    expect(queryByRole('alert')).not.toBeInTheDocument();
+  });
+
+  // Note: Full functionality testing is covered by Storybook stories
+  // which test the component with Flux integration
+});
